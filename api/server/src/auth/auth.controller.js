43b6a279122eba7da862c5d931c94a7bdff1612e
andysociolla <andy.juliadi@sociolla.com>
2019-08-18 00:37:40 +0700

    update

--- api/server/src/auth/auth.controller.js
+++ api/server/src/auth/auth.controller.js
@@ -21,20 +21,7 @@ exports.login = (req, res, next) => {
         });
     }
 
-    return passport.authenticate('login', { session: false }, (err, passportUser, info) => {
-        if(err) {
-          return next(err);
-        }
-
-        if(passportUser) {
-          const user = passportUser;
-          user.token = passportUser.generateJWT();
-
-          return res.json({ user: user.toAuthJSON() });
-        }
-
-        return res.status(422).json(info);
-    }) (req, res, next);
+    return res.json({ user: req.user.toAuthJSON() });
 };
 
 //User logout
