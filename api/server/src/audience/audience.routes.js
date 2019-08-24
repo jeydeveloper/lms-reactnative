67fd7f991a0c6d48730ae00813b51bcf3130ef6d
andysociolla <andy.juliadi@sociolla.com>
2019-08-24 10:53:39 +0700

    update comment route

--- api/server/src/audience/audience.routes.js
+++ api/server/src/audience/audience.routes.js
@@ -2,18 +2,18 @@ module.exports = (app) => {
     const audiences = require('./audience.controller.js');
     const auth = require('../library/auth.js');
 
-    // Create a new User
+    // Create a new Audience
     app.post('/audiences', auth.optional, audiences.create);
 
-    // Retrieve all Users
+    // Retrieve all Audiences
     app.get('/audiences', auth.required, audiences.findAll);
 
-    // Retrieve a single User with audienceId
+    // Retrieve a single Audience with audienceId
     app.get('/audiences/:audienceId', auth.required, audiences.findOne);
 
-    // Update a User with audienceId
+    // Update a Audience with audienceId
     app.put('/audiences/:audienceId', auth.required, audiences.update);
 
-    // Delete a User with audienceId
+    // Delete a Audience with audienceId
     app.delete('/audiences/:audienceId', auth.required, audiences.delete);
 }
