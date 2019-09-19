5d08f1a653eaa69cf879349f5a5d860f9160bf7d
andysociolla <andy.juliadi@sociolla.com>
2019-09-20 01:27:46 +0700

    update

--- ui-react/ios/FelixPro/AppDelegate.m
+++ ui-react/ios/FelixPro/AppDelegate.m
@@ -18,7 +18,7 @@
   [GMSServices provideAPIKey:@"AIzaSyDgdfbt2kBGOTm-Y6ESIC-D6uLhcshrHfo"];
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"FelixPro"
